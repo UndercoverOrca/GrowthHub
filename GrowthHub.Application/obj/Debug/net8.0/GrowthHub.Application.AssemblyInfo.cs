f30b037907c217787ed7f2760dc024e695b88f54
Tim van der Nol <tim.nol98@gmail.com>
2024-03-06 18:14:06 +0100

    [06/02/2024] - Add initial projects

--- GrowthHub.Application/obj/Debug/net8.0/GrowthHub.Application.AssemblyInfo.cs
+++ GrowthHub.Application/obj/Debug/net8.0/GrowthHub.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrowthHub.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43136872c6c672bde9cf0a758adc48baead8d54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrowthHub.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrowthHub.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GrowthHub.WebUi/obj/Debug/net8.0/GrowthHub.WebUi.AssemblyInfo.cs
+++ GrowthHub.WebUi/obj/Debug/net8.0/GrowthHub.WebUi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrowthHub.WebUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43136872c6c672bde9cf0a758adc48baead8d54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrowthHub.WebUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrowthHub.WebUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

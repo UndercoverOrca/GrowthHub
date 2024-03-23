ea9c277beed2cf0e6878b85e3935d44cadede8fc
Tim van der Nol <tim.nol98@gmail.com>
2024-03-23 18:29:15 +0100

    [23/03/2024] - Add files

--- GrowthHub.Tests/obj/Debug/net8.0/GrowthHub.Tests.AssemblyInfo.cs
+++ GrowthHub.Tests/obj/Debug/net8.0/GrowthHub.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrowthHub.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865345c91d7277bc87f58d41b6ef43e4565261d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e0b10dfb8b40124d6e6a060a7931b2fc4a62c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrowthHub.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrowthHub.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

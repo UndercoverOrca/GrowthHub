c977b66dec4f8a8ace786ab0de19f6727519f552
Tim van der Nol <tim.vandernol@vicompany.nl>
2024-05-16 14:24:07 +0200

    [main] - Add Google Authentication

--- GrowthHub.Domain/obj/Debug/net8.0/GrowthHub.Domain.AssemblyInfo.cs
+++ GrowthHub.Domain/obj/Debug/net8.0/GrowthHub.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrowthHub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6246aa57f6f1552134a12d4d7c8009ff5559efb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d326b9f6cdfc601cdf359639fe37197e3e2060d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrowthHub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrowthHub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
